00b58f12b326c147320ccbf0dea0ef83a7a65ff6
Sergio Tanzilli <tanzilli@acmesystems.it>
2018-05-29 19:26:05 +0200

    Backup

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import lcd
 import time
 import keys
 from acmepins import GPIO 
+import socket
+import ds18b20
+
+def myip():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+	local_ip_address = s.getsockname()[0]
+	return local_ip_address
 
 display=lcd.lcd()
 display.backlight_on()
@@ -9,11 +17,12 @@ key=keys.KEYS()
 rele=GPIO("J4.29","OUTPUT")
 
 STATE_INIT=0
+STATE_WELCOME=1
+STATE_TEMP=2
 STATE_TEST_RELAY=3
 STATE_BACKLIGHT=4
-
-# 0 = Init
-# 1 = Welcome message
+STATE_MYIP=5
+LAST_STATE=5
 
 current_state=0
 next_state=1
@@ -26,7 +35,7 @@ while True:
 			if current_state>1:
 				next_state=current_state-1
 		if a==key.KEY_RIGHT:
-			if current_state<4:
+			if current_state<LAST_STATE:
 				next_state=current_state+1
 				
 		if a==key.KEY_ESC:
@@ -40,34 +49,39 @@ while True:
 				rele.on()
 			if current_state==STATE_BACKLIGHT:
 				display.backlight_on()
-				
 		continue
 		
-	if next_state==1:
+	if next_state==STATE_WELCOME:
 		display.clear()	
 		display.putstring("DS485")		
 		display.setcurpos(0,1)
-		display.putstring("Ver 0.1")
-		current_state=1
+		display.putstring("Ver 0.02")
+		current_state=next_state
 
-	if next_state==2:
+	if next_state==STATE_TEMP:
 		display.clear()	
 		display.putstring("TEMP")		
-		current_state=2
+		current_state=next_state
+
+	if next_state==STATE_MYIP:
+		display.clear()	
+		display.putstring("IP address")
+		display.setcurpos(0,1)
+		display.putstring(myip())
+		current_state=next_state
 
 	if next_state==STATE_TEST_RELAY:
 		display.clear()	
 		display.putstring("TEST RELE")
 		display.setcurpos(0,1)
 		display.putstring("ESC=OFF    OK=ON")
-		current_state=STATE_TEST_RELAY
+		current_state=next_state
 
 	if next_state==STATE_BACKLIGHT:
 		display.clear()	
 		display.putstring("BACKLIGHT")
 		display.setcurpos(0,1)
 		display.putstring("ESC=OFF    OK=ON")
-		current_state=STATE_BACKLIGHT
-
+		current_state=next_state
 
 	continue
--- sensors.py
+++ sensors.py
@@ -1,8 +1,4 @@
 import ds18b20
 
-sensors = []
-for sensor_id in ds18b20.get_available_sensors():
-    sensors.append(sensor_id)
-
-for sensor in sensors:
-    print("Sensor %s has temperature %.2f" % (sensor, ds18b20.get_temperature(sensor)))
+for sensor in ds18b20.get_available_sensors():
+    print("Sensor %s has temperature %.2f" % (sensor, ds18b20.get_temperature(sensor)))    
