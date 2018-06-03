6f0428bf79ec3c70ae97b42ffc2436471dc2381d
Sergio Tanzilli <tanzilli@acmesystems.it>
2018-06-03 20:54:35 +0200

    Multinode reading

--- readtemp.py
+++ readtemp.py
@@ -1,27 +1,30 @@
 import rs485	
 import time
 
+node_addrs=[2,3]
+
 # Apre la linea seriale sulla RS485
 link=rs485.Link("/dev/ttyUSB0")
 
 while True:
-	# Invia la richiesta di lettura al nodo n.2
-	link.send(rs485.Packet(2))
+	for node_addr in node_addrs:	
+		# Invia la richiesta di lettura al nodo n.2
+		link.send(rs485.Packet(node_addr))
 
-	# Aspetta la risposta
-	reply=link.receive()
+		# Aspetta la risposta
+		reply=link.receive()
 
-	# Controlla se e' arrivata la risposa
-	if reply==None:
-		print "Timeout"
-		time.sleep(1)
-		continue
-	else:
-		sensors=reply.get()
-		for sensor in sensors:
-			print "Id=%s Temp=%.1f" % (sensor,sensors[sensor])
-		print " "	
-		time.sleep(1)
+		# Controlla se e' arrivata la risposa
+		if reply==None:
+			print "Timeout"
+			time.sleep(1)
+			continue
+		else:
+			sensors=reply.get()
+			for sensor in sensors:
+				print "Node=%d Id=%s Temp=%.2f" % (node_addr,sensor,sensors[sensor])
+			print " "	
+			time.sleep(1)
 
 
 
